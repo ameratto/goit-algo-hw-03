346d7f5d3cdd7d3fbfbac1a06c6e1fd85dab08cc
ameratto <q27www@gmail.com>
2026-06-09 17:38:13 +0300

    Трете завдання

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from datetime import datetime, date
 import random
 from typing import Any
+import re
+from unittest import case
 
 
 # Функція приймає один параметр: date — рядок, що представляє дату у форматі 'РРРР-ММ-ДД' (наприклад, '2020-10-09').
@@ -11,6 +13,7 @@ def get_days_from_today(user_date: str) -> int:
     today = date.today().toordinal()
     return datetime.strptime(user_date, '%Y-%m-%d').toordinal() - today
 
+
 def get_numbers_ticket(val_quantity: int, val_min: int = 1, val_max: int = 1000) -> list[Any]:
     set_of_numbers = set()
     if val_min < 1 or val_max > 1000:
@@ -20,16 +23,41 @@ def get_numbers_ticket(val_quantity: int, val_min: int = 1, val_max: int = 1000)
         set_of_numbers.add(random.randint(val_min, val_max))
     return sorted(list(set_of_numbers))
 
-desired_date = '2026-06-15'
-var_min = 1
-var_max = 156
-quantity = 5
 
+def normalize_phone(phone_number: str):
+    normalize_phone_number = re.findall(r"\d{10}$",re.sub(r'\D+', '', phone_number))
+    return '+38' + normalize_phone_number[0]
+    # match phone_code:
+    #     case 'ukr':
+    #         return '+38' + phone_number
+    #     case 'jp':
+    #         return '+81' + phone_number
+    #     case 'en':
+    #         return '+1264' + phone_number
+    #     case _:
+    #         return phone_number
+
+
+desired_date = '2026-06-15'
 # print("Завдання 1:")
 # print(f"Результат виконання функції get_days_from_today : \n\
 # Кількість днів від сьогоднішньої дати {date.today()} до вказаної {desired_date} = \
 # {get_days_from_today(desired_date)}")
 
+
+var_min = 1
+var_max = 156
+quantity = 5
 # print("Завдання 2:")
 # print(f"Результат виконання функції get_numbers_ticket з наступними параметрами var_min = {var_min}, var_max = {var_max}, quantity = {quantity}: \n\
 # {get_numbers_ticket(quantity, var_min, var_max)}")
+
+
+phone_number_strings = ["    +38(050)123-32-34",
+                "     0503451234",
+                "(050)8889900",
+                "38050-111-22-22",
+                "38050 111 22 11   "]
+# print("Завдання 3:")
+# for number in phone_number_strings:
+#     print(f"{normalize_phone(number)}")
