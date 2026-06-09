3399bf80b6688df3f98c704d0396ddd7f53275f9
ameratto <q27www@gmail.com>
2026-06-09 15:37:07 +0300

    Друге завдання, функція get_numbers_ticket(quantity, min, max)

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from datetime import datetime, date
+import random
+from typing import Any
 
 
 # Функція приймає один параметр: date — рядок, що представляє дату у форматі 'РРРР-ММ-ДД' (наприклад, '2020-10-09').
@@ -9,9 +11,25 @@ def get_days_from_today(user_date: str) -> int:
     today = date.today().toordinal()
     return datetime.strptime(user_date, '%Y-%m-%d').toordinal() - today
 
+def get_numbers_ticket(val_quantity: int, val_min: int = 1, val_max: int = 1000) -> list[Any]:
+    set_of_numbers = set()
+    if val_min < 1 or val_max > 1000:
+        print(f"Невірне значення параметра min або max: {val_min if val_min <= 0 else val_max}")
+        return list()
+    while len(set_of_numbers) <= val_quantity:
+        set_of_numbers.add(random.randint(val_min, val_max))
+    return sorted(list(set_of_numbers))
 
 desired_date = '2026-06-15'
+var_min = 1
+var_max = 156
+quantity = 5
 
-print(f"Результат виконання функції get_days_from_today : \n\
-Кількість днів від сьогоднішньої дати {date.today()} до вказаної {desired_date} = \
-{get_days_from_today(desired_date)}")
+# print("Завдання 1:")
+# print(f"Результат виконання функції get_days_from_today : \n\
+# Кількість днів від сьогоднішньої дати {date.today()} до вказаної {desired_date} = \
+# {get_days_from_today(desired_date)}")
+
+# print("Завдання 2:")
+# print(f"Результат виконання функції get_numbers_ticket з наступними параметрами var_min = {var_min}, var_max = {var_max}, quantity = {quantity}: \n\
+# {get_numbers_ticket(quantity, var_min, var_max)}")
