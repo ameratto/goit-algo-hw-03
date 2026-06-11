aa132365372b042a6625e60dc6ba163729f2a302
ameratto <q27www@gmail.com>
2026-06-11 15:43:07 +0300

    Виправлення зауважень

--- main.py
+++ main.py
@@ -35,12 +35,19 @@ def get_days_from_today(user_date: str):
 
 def get_numbers_ticket(val_min: int, val_max: int, val_quantity: int) -> list[Any]:
     set_of_numbers = set()
-    if val_min < 1 or val_max > 1000:
-        print(f"Невірне значення параметра min або max: {val_min if val_min <= 0 else val_max}")
+    if val_min <= 1 or val_max >= 1000:
+        #print(f"Невірне значення параметра min або max: {val_min if val_min <= 0 else val_max}")
         return list()
-    while len(set_of_numbers) < val_quantity:
-        set_of_numbers.add(random.randint(val_min, val_max))
-    return sorted(list(set_of_numbers))
+    elif val_min > val_max:
+        #print(f"Мінімальне число {val_min} не може бути більше за максимальне {val_max}")
+        return list()
+    elif val_max - val_min < quantity:
+        #print(f"Кількість запрошуваних чисел {quantity} більша за вказаний діапазон [{val_min}; {val_max}]")
+        return list()
+    else:
+        while len(set_of_numbers) <= val_quantity:
+            set_of_numbers.add(random.randint(val_min, val_max))
+        return sorted(list(set_of_numbers))
 
 
 def normalize_phone(phone_number: str):
@@ -70,12 +77,12 @@ desired_dates = ["2026-08-11",
 
 # ----- #
 
-var_min = 5
-var_max = 1560
-quantity = 5
-# print("Завдання 2:")
-# print(f"Результат виконання функції get_numbers_ticket з наступними параметрами var_min = {var_min}, var_max = {var_max}, quantity = {quantity}: \n\
-# {get_numbers_ticket(var_min, var_max, quantity)}")
+var_min = 10
+var_max = 14
+quantity = 6
+print("Завдання 2:")
+print(f"Результат виконання функції get_numbers_ticket з наступними параметрами var_min = {var_min}, var_max = {var_max}, quantity = {quantity}: \n\
+{get_numbers_ticket(var_min, var_max, quantity)}")
 
 # ----- #
 
