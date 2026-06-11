105f9ea9f98dafa58b713b1845eed4bd2b37db56
ameratto <q27www@gmail.com>
2026-06-11 15:43:53 +0300

    Виправлення зауважень

--- main.py
+++ main.py
@@ -80,9 +80,9 @@ desired_dates = ["2026-08-11",
 var_min = 10
 var_max = 14
 quantity = 6
-print("Завдання 2:")
-print(f"Результат виконання функції get_numbers_ticket з наступними параметрами var_min = {var_min}, var_max = {var_max}, quantity = {quantity}: \n\
-{get_numbers_ticket(var_min, var_max, quantity)}")
+# print("Завдання 2:")
+# print(f"Результат виконання функції get_numbers_ticket з наступними параметрами var_min = {var_min}, var_max = {var_max}, quantity = {quantity}: \n\
+# {get_numbers_ticket(var_min, var_max, quantity)}")
 
 # ----- #
 
