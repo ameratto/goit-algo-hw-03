c49702d509639e36a55b3f5cdb5ca80c9e184e10
ameratto <q27www@gmail.com>
2026-06-11 21:51:13 +0300

    Виправлення зауважень

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def get_numbers_ticket(val_min: int, val_max: int, val_quantity: int) -> list[An
     elif val_min > val_max:
         #print(f"Мінімальне число {val_min} не може бути більше за максимальне {val_max}")
         return list()
-    elif val_max - val_min < quantity:
+    elif (val_max - val_min) < val_quantity:
         #print(f"Кількість запрошуваних чисел {quantity} більша за вказаний діапазон [{val_min}; {val_max}]")
         return list()
     else:
@@ -80,9 +80,9 @@ desired_dates = ["2026-08-11",
 var_min = 10
 var_max = 14
 quantity = 6
-# print("Завдання 2:")
-# print(f"Результат виконання функції get_numbers_ticket з наступними параметрами var_min = {var_min}, var_max = {var_max}, quantity = {quantity}: \n\
-# {get_numbers_ticket(var_min, var_max, quantity)}")
+print("Завдання 2:")
+print(f"Результат виконання функції get_numbers_ticket з наступними параметрами var_min = {var_min}, var_max = {var_max}, quantity = {quantity}: \n\
+{get_numbers_ticket(var_min, var_max, quantity)}")
 
 # ----- #
 
